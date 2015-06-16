b3718de89937fb8e3ec7ee1f891589e9b383d351
Hayk Saakian <hayk@skyrealre.com>
2015-06-15 22:45:57 -0700

    avoid loading if not needed

--- popup.js
+++ popup.js
@@ -3,7 +3,6 @@ var popup_in = 1000
 var showpopup = localStorage.getItem('popupbeta');
 var popup_url = "http://innovativemen.com/popup/"
 
-
 function loadjscssfile(filename, filetype){
     if (filetype=="js"){ //if filename is a external JavaScript file
         var fileref=document.createElement('script')
@@ -30,41 +29,42 @@ function checkloadjscssfile(filename, filetype){
     }
     else
         alert("file already added!")
-}
+  }
 
-if(typeof $ == 'undefined'){
-    checkloadjscssfile("https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js", "js")
-}
- 
-checkloadjscssfile("http://dimsemenov-static.s3.amazonaws.com/dist/jquery.magnific-popup.min.js", "js") //success
-checkloadjscssfile("http://dimsemenov-static.s3.amazonaws.com/dist/magnific-popup.css", "css") //redundant file, so file not added
+function initPopupJS () {
+  if(typeof $ == 'undefined'){
+      checkloadjscssfile("https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js", "js")
+  }
+   
+  checkloadjscssfile("http://dimsemenov-static.s3.amazonaws.com/dist/jquery.magnific-popup.min.js", "js") //success
+  checkloadjscssfile("http://dimsemenov-static.s3.amazonaws.com/dist/magnific-popup.css", "css") //redundant file, so file not added
 
 
-function initPopup () {
-  $(document).ready(function() {
+  function initPopup () {
+    $(document).ready(function() {
 
-    setTimeout(function (){
-      if(!showpopup){
-          return
+      setTimeout(openPopup, popup_in)
+    })
+  }
+  setTimeout(initPopup, (typeof $ == 'undefined') ? 200 : 0)
+
+
+  function openPopup(){
+    $.magnificPopup.open({
+      type: 'iframe',
+      items: {
+          src: popup_url,        
+      },
+      iframe: {
+         markup: '<div class="mfp-iframe-scaler">'+
+                    '<div class="mfp-close"></div>'+
+                    '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+                  '</div>'
       }
-      openPopup()
-    }, popup_in)
-  })
+    })
+  }
 }
-setTimeout(initPopup, (typeof $ == 'undefined') ? 200 : 0)
 
-
-function openPopup(){
-  $.magnificPopup.open({
-    type: 'iframe',
-    items: {
-        src: popup_url,        
-    },
-    iframe: {
-       markup: '<div class="mfp-iframe-scaler">'+
-                  '<div class="mfp-close"></div>'+
-                  '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-                '</div>'
-    }
-  })
+if(showpopup){
+  initPopupJS()
 }
